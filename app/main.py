29e89c65da7f2ac4984bc440949ccb5cacd2c9dc
sky-chronos <gomdoribooh@gmail.com>
2026-01-18 22:12:51 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
 
+from fastapi import FastAPI
+from app.kcsc_client import get_codeviewer
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"ok": True}
+
+@app.get("/kcsc/codeviewer/{Type}/{Code}")
+async def kcsc_codeviewer(Type: str, Code: str):
+    return await get_codeviewer(Type, Code)
